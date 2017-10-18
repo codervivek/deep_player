5f9cc467adc8193fc8a091180b1f251bd35e848e
Vivek Raj <raj.vivek.151297@gmail.com>
2017-10-19 01:28:36 +0530

    'try'

---
+++ deep_player/main.py
@@ -0,0 +1,6 @@
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "server.settings")
+
+from django.core.management import call_command
+from django.core.wsgi import get_wsgi_application 
+application = get_wsgi_application()
+call_command('runserver',  '127.0.0.1:8000')
