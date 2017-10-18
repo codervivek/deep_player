2733dd1612e1abec4bcfc713a9cc470294adc23a
Vivek Raj <raj.vivek.151297@gmail.com>
2017-10-18 22:52:06 +0530

    added eb pkg config1

--- deep_player/settings.py
+++ deep_player/settings.py
@@ -74,23 +74,35 @@ WSGI_APPLICATION = 'deep_player.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
+if 'RDS_DB_NAME' in os.environ:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': os.environ['RDS_DB_NAME'],
+            'USER': os.environ['RDS_USERNAME'],
+            'PASSWORD': os.environ['RDS_PASSWORD'],
+            'HOST': os.environ['RDS_HOSTNAME'],
+            'PORT': os.environ['RDS_PORT'],
+        }
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'deep_player',
+            'USER': 'vivekraj',
+            'PASSWORD': 'heysiri',
+            'HOST': 'localhost',
+            'PORT': '',
+        }
+    }
+
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'deep_player',
-#         'USER': 'vivekraj',
-#         'PASSWORD': 'heysiri',
-#         'HOST': 'localhost',
-#         'PORT': '',
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 #     }
 # }
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
 
 
 # Password validation
