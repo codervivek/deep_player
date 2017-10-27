efb5e9674d4170133483c02429cb00d1f6fcd0e4
Vivek Raj <raj.vivek.151297@gmail.com>
2017-10-27 19:32:48 +0530

    sqlserver

--- deep_player/settings.py
+++ deep_player/settings.py
@@ -86,23 +86,24 @@ if 'RDS_DB_NAME' in os.environ:
         }
     }
 else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql_psycopg2',
-            'NAME': 'deep_player',
-            'USER': 'vivekraj',
-            'PASSWORD': 'heysiri',
-            'HOST': 'localhost',
-            'PORT': '',
-        }
-    }
+	DATABASES = {
+	    'default': {
+	        'ENGINE': 'django.db.backends.sqlite3',
+	        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+	    }
+	}
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
+    # DATABASES = {
+    #     'default': {
+    #         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    #         'NAME': 'deep_player',
+    #         'USER': 'vivekraj',
+    #         'PASSWORD': 'heysiri',
+    #         'HOST': 'localhost',
+    #         'PORT': '',
+    #     }
+    # }
+
 
 
 # Password validation
