b33c5b522eea24516a8bcfb433d900a4eafb3a40
Pavan Kumar <pavan71198@users.noreply.github.com>
2018-03-06 02:05:13 +0530

    a

--- deep_player/settings.py
+++ deep_player/settings.py
@@ -142,7 +142,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, "..", "www", "staticfiles")
+STATIC_ROOT = os.path.join(BASE_DIR, "..", "www", "static")
 STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "..", "www", "media")
 MEDIA_URL = '/media/'
